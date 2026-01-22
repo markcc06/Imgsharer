f2bc3334cc7916b56bd130369891b7a804ac8a9c
张文峰 <hua356463@gmail.com>
2026-01-22 18:34:52 +0800

    seo: 308 redirect selfie-makeup-use-case

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,15 @@ const nextConfig = {
       "*": ["**/.git/**", "**/.next/cache/**"],
     },
   },
+  async redirects() {
+    return [
+      {
+        source: "/blog/selfie-makeup-use-case",
+        destination: "/blog/how-to-unblur-a-picture-on-iphone-selfie-makeup",
+        permanent: true, // uses 308
+      },
+    ]
+  },
 }
 
 export default nextConfig
